7d1b5721ed23366c50d51480b78488b40762ebb6
fitz0017 <fitz.kevin@gmail.com>
2020-10-04 19:03:50 +0000

    set up basic cli syntax

---
+++ assault/http.py
@@ -0,0 +1,17 @@
+import asyncio
+
+def fetch():
+    """Make a request, and fetch the results"""
+    pass
+
+def worker(name, queue, results):
+    """Worker function to execute requests from a queue and then add results to results list"""
+    pass
+
+def distribute_work(url, requests, concurrency, results):
+    """Divide the work into matches and collect final results """
+    pass
+
+def assault(url, requests, concurrency):
+    results =[]
+    pass
